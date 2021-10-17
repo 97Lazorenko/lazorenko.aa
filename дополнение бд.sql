d518ea507870411bd093be522031497b2667e06e
97Lazorenko <97Lazorenko97@gmail.com>
2021-10-18 02:26:20 +0700

    Учебный проект Лазренко Александр

--- дополнение бд.sql
+++ дополнение бд.sql
@@ -9,6 +9,14 @@ create table patient_docs (
         patient_id number references LAZORENKO_AL.patient(patient_id)
         );
 
+insert into LAZORENKO_AL.patient_docs(passport, inipa, itn, patient_id)
+values ('3217 475785', '999888777666555', '888777666555', 1);
+insert into LAZORENKO_AL.patient_docs(passport, inipa, itn, patient_id)
+values ('3219 758497', '999777555333111', '111222333444', 2);
+insert into LAZORENKO_AL.patient_docs(passport, inipa, itn, patient_id)
+values ('3218 460985', '222444666888777', '000888777555', 3);
+
+
 alter table LAZORENKO_AL.doctor drop column qualification;
 
 create table doctors_info (
