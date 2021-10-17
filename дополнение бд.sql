52bec127e3d452e4b489c22b39684746f2f1b63f
97Lazorenko <97Lazorenko97@gmail.com>
2021-10-18 01:59:45 +0700

    Учебный проект Лазренко Александр

--- дополнение бд.sql
+++ дополнение бд.sql
@@ -1,3 +1,4 @@
+alter table LAZORENKO_AL.patient drop column docs;
 
 create table patient_docs (
     docs_id number generated always as identity (
@@ -7,4 +8,17 @@ create table patient_docs (
         itn varchar2(100),
         patient_id number references LAZORENKO_AL.patient(patient_id)
         );
+
+alter table LAZORENKO_AL.doctor drop column qualification;
+
+create table doctors_info (
+    doctor_info_id number generated always as identity (
+        start with 1 maxvalue 999999999999 minvalue 1 nocycle nocache noorder) primary key,
+        education varchar2(500) not null,
+        qualification number not null,
+        salary number not null,
+        rating number,
+        reviews varchar2(500),
+        doctor_id number references LAZORENKO_AL.doctor(doctor_id) not null
+);
 
