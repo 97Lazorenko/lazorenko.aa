e5768ade7d1a7c1772e6ed8c6029f4bdd70ec513
97Lazorenko <97Lazorenko97@gmail.com>
2021-10-18 13:50:10 +0700

    Учебный проект Лазоренко Александр

--- sql4.sql
+++ sql4.sql
@@ -1,10 +1,14 @@
-select d.name, s.name, d.qualification,
-CASE d.zone_id
-     WHEN 2 THEN 1 --участок пациента с zone_id=2
-     ELSE 0
-END AS участок_пациента
+select d.name, s.name, di.qualification,
+case
+     when d.zone_id=2 then 1 --участок пациента с zone_id=2
+     else 0
+end as участок_пациента
 from doctor d inner join doctor_spec using(doctor_id)
     inner join specialisation s using(spec_id)
+    inner join doctors_info di using(doctor_id)
     inner join hospital using(hospital_id)
 where 5 = hospital_id and d.dismiss_date is null
-order by участок_пациента desc, qualification desc;
+order by участок_пациента desc, di.qualification desc;
+
+
+
