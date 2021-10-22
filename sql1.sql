2e7399e979782844f5e1fc738a961db796f323d3
97Lazorenko <97Lazorenko97@gmail.com>
2021-10-22 13:10:57 +0700

    Учебный проект Лазоренко Александр

--- sql1.sql
+++ sql1.sql
@@ -1,2 +1,4 @@
+
 select c.name, r.name
 from CITY c inner join REGION r USING(region_id);
+
--- sql3.sql
+++ sql3.sql
@@ -1,12 +1,12 @@
 select h.name, a.name, count(doctor_id) as количество_врачей, o.name, w.end_time,
  case
-    when w.end_time>TO_CHAR(sysdate, 'hh24:mi') then 1
+    when w.end_time>TO_CHAR(sysdate, 'hh24:mi:ss') then 1
     else 0
 end as сейчас_открыто
-from hospital h inner join available a using(availability_id)
-    inner join ownership_type o using(ownership_type_id) inner join work_time w using(hospital_id)
+from hospital h left join work_time w using(hospital_id)
+    inner join ownership_type o using(ownership_type_id)
     inner join doctor using(hospital_id) inner join doctor_spec using(doctor_id)
-    inner join specialisation using(spec_id)
-where 2=spec_id and h.delete_from_the_sys is null and w.day in (select to_char(sysdate,'day') from dual)
+    inner join available a using(availability_id)
+where 2=spec_id and h.delete_from_the_sys is null and w.day in to_char(sysdate, 'd')
 group by h.name, a.name, o.name, w.end_time
 order by o.name desc, количество_врачей desc, сейчас_открыто desc;
