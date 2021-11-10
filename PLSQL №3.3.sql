9f011e5c56639bfad8a62e6f0b2811f37fc2d4cb
97Lazorenko <97Lazorenko97@gmail.com>
2021-11-11 00:23:54 +0700

    Учебный проект Лазоренко Александр

--- PLSQL №3.3.sql
+++ PLSQL №3.3.sql
@@ -5,14 +5,17 @@ create or replace function write_to_records(
 p_patient_id in number,
 p_ticket_id in number)
 return lazorenko_al.records.record_id%type as
- v_record_id lazorenko_al.records.record_id%type;
+v_record_id lazorenko_al.records.record_id%type;
 begin
     insert into lazorenko_al.records(record_id, record_stat_id, patient_id, ticket_id)
     values (default, 1, p_patient_id, p_ticket_id)
+
     returning record_id into v_record_id;
+
     update lazorenko_al.ticket t set t.ticket_stat_id=2 where t.ticket_id=p_ticket_id;
+
     commit;
-return v_record_id;
+    return v_record_id;
 end;
 
 declare
@@ -24,8 +27,14 @@ end;
 
 --ФУНКЦИЯ ПРОВЕРКИ УСЛОВИЙ ПЕРЕД ЗАПИСЬЮ
 
-create or replace function lazorenko_al.check_for_accept(p_ticket_id in number, p_patient_id in number,
-p_spec_id in number, p_doctor_id in number, p_hospital_id in number, out_messages out varchar2
+create or replace function lazorenko_al.check_for_accept(
+p_ticket_id in number,
+p_patient_id in number,
+p_spec_id in number,
+p_doctor_id in number,
+p_hospital_id in number,
+p_lazy_check in boolean default false,
+out_messages out varchar2
 )
 return boolean
 as
@@ -39,6 +48,7 @@ begin
     )) then v_result:=false;
     out_messages:=out_messages||chr(10)
     ||'несоответствие врача и специальности или врача и больницы или выбранного талона и врача';
+    if (p_lazy_check) then return v_result; end if;
     end if;
     if (not lazorenko_al.patient_doc_check(
     p_patient_id => p_patient_id
@@ -89,12 +99,13 @@ end if;
     return v_result;
     end;
 
-    declare
+declare
    v_result number;
    v_messages varchar2(500);
 begin
-    v_result :=sys.diutil.bool_to_int(lazorenko_al.check_for_accept(33, 2, 3, 3, 5, v_messages));
-    DBMS_OUTPUT.PUT_LINE(v_messages);
+    v_result :=sys.diutil.bool_to_int(lazorenko_al.check_for_accept(
+    27, 7, 6, 3, 5, true,v_messages));
+    dbms_output.put_line(v_messages);
 end;
 
 --ФУНКЦИЯ ЗАПИСИ С ПРОВЕРКОЙ УСЛОВИЯ
@@ -105,27 +116,43 @@ v_patient_id number,
 v_spec_id number,
 v_doctor_id number,
 v_hospital_id number,
+v_lazy_check boolean,
 v_messages in out varchar2,
 v_result out number
 ) return  lazorenko_al.records.record_id%type as
 v_record_id lazorenko_al.records.record_id%type;
 begin
  if (lazorenko_al.check_for_accept(
-            p_ticket_id => v_ticket_id, p_patient_id => v_patient_id, p_spec_id => v_spec_id,
-            p_doctor_id => v_doctor_id, p_hospital_id => v_hospital_id, out_messages => v_messages))
- then v_record_id:=lazorenko_al.write_to_records(p_patient_id => v_patient_id, p_ticket_id => v_ticket_id);
+            p_ticket_id => v_ticket_id,
+            p_patient_id => v_patient_id,
+            p_spec_id => v_spec_id,
+            p_doctor_id => v_doctor_id,
+            p_hospital_id => v_hospital_id,
+            p_lazy_check => v_lazy_check,
+            out_messages => v_messages))
+ then v_record_id:=lazorenko_al.write_to_records(p_patient_id => v_patient_id,
+                                                 p_ticket_id => v_ticket_id);
+
  dbms_output.put_line(v_record_id ||' - '||'запись осуществлена успешно');
- else v_result:=sys.diutil.bool_to_int(lazorenko_al.check_for_accept(v_ticket_id, v_patient_id, v_spec_id, v_doctor_id, v_hospital_id, v_messages));
- DBMS_OUTPUT.PUT_LINE(v_messages);
+
+ else v_result:=sys.diutil.bool_to_int(lazorenko_al.check_for_accept(v_ticket_id,
+                                                                        v_patient_id,
+                                                                        v_spec_id,
+                                                                        v_doctor_id,
+                                                                        v_hospital_id,
+                                                                        v_lazy_check,
+                                                                        v_messages));
+ dbms_output.put_line(v_messages);
 end if;
 return v_result;
 end;
 
 --ПРИМЕНЕНИЕ МЕТОДА ЗАПИСИ
-    declare
+declare
    v_messages varchar2(500);
    v_result number;
 begin
-    v_result:=lazorenko_al.accept_record_by_rules(27, 2, 6, 31, 5,
+    v_result:=lazorenko_al.accept_record_by_rules(
+    27, 2, 6, 31, 5, false,
     v_messages, v_result);
 end;
