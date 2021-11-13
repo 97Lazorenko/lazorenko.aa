71ed2e3a958b801e3bcdce637ed011a2e0e1399a
97Lazorenko <97Lazorenko97@gmail.com>
2021-11-13 12:00:31 +0700

    Учебный проект Лазоренко Александр

--- test1.sql
+++ test1.sql
@@ -1,4 +1,4 @@
-create or replace package lazorenko_al.pkg_const_for_write_or_cancel
+create or replace package lazorenko_al.pkg_write_or_cancel
 as
     c_constant_1 constant number := 1;
 
@@ -6,17 +6,18 @@ as
 
     v_record_id lazorenko_al.records.record_id%type;
 
+    v_record_id number;
+        function cancel_record(
+    p_ticket_id in number)
+    return number;
+
     function write_to_records(
          p_patient_id in number,
          p_ticket_id in number)
     return lazorenko_al.records.record_id%type;
-
-    function lazorenko_al.cancel_record(
-    p_ticket_id in number)
-return v_record_id number;
 end;
 
-    create or replace package body lazorenko_al.pkg_const_for_write_or_cancel
+create or replace package body lazorenko_al.pkg_write_or_cancel
 as
     function write_to_records(
          p_patient_id in number,
@@ -27,20 +28,41 @@ as
 
     begin
     insert into lazorenko_al.records(record_id, record_stat_id, patient_id, ticket_id)
-    values (default, LAZORENKO_AL.PKG_CONST_FOR_WRITE_OR_CANCEL.c_write_record_stat_id_constant, p_patient_id, p_ticket_id)
+    values (default, lazorenko_al.pkg_const_for_write_or_cancel.c_constant_1, p_patient_id, p_ticket_id)
     returning record_id into v_record_id;
 
-    update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.PKG_CONST_FOR_WRITE_OR_CANCEL.c_write_update_ticket_stat_id_constant where t.ticket_id=p_ticket_id;
+    update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_const_for_write_or_cancel.c_constant_2 where t.ticket_id=p_ticket_id;
 
     commit;
 
-return v_record_id;
-end;
+    return v_record_id;
+    end;
+
+    function cancel_record(
+    p_ticket_id in number)
+    return  number  as
+    v_record_id number;
+
+    begin
+    update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_const_for_write_or_cancel.c_constant_1 where t.ticket_id=p_ticket_id;
+
+    update lazorenko_al.records r set r.record_stat_id=lazorenko_al.pkg_const_for_write_or_cancel.c_constant_2 where r.ticket_id=p_ticket_id;
+
+    commit;
+
+    return v_record_id;
+    end;
 
 end;
 
 declare
 v_record number;
 begin
-v_record:=lazorenko_al.pkg_const_for_write_or_cancel.write_to_records(1,33);
+v_record:=lazorenko_al.pkg_write_or_cancel.write_to_records(1,33);
+end;
+
+declare
+v_record number;
+begin
+v_record:=lazorenko_al.pkg_write_or_cancel.cancel_record(33);
 end;
