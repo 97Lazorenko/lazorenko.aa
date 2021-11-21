02c7802a9814e2bdf2dc4050582747a77f11f1f7
97Lazorenko <97Lazorenko97@gmail.com>
2021-11-22 00:03:41 +0700

    Учебный проект Лазоренко Александр

---
+++ test 6.2.sql
@@ -0,0 +1,75 @@
+create or replace package lazorenko_al.pkg_fail
+as
+    procedure fail_method(
+        p_value varchar2,
+        p_need_handle boolean
+    );
+end;
+/
+
+
+
+create or replace package body lazorenko_al.pkg_fail
+as
+    procedure fail_method(
+        p_value varchar2,
+        p_need_handle boolean
+    )
+    as
+    begin
+
+        if (p_need_handle) then
+            begin
+                insert into lazorenko_al.test(value)
+                values (p_value);
+
+            exception
+                when others then
+
+                    lazorenko_al.add_error_log(
+                        $$plsql_unit_owner||'.'||$$plsql_unit||'.'||utl_call_stack.subprogram(1)(2),
+                        '{"error":"' || sqlerrm
+                        ||'","value":"' || p_value
+                        ||'","backtrace":"' || dbms_utility.format_error_backtrace()
+                        ||'"}'
+                    );
+
+                    rollback;
+
+            end;
+        else
+            insert into lazorenko_al.test(value)
+            values (p_value);
+
+        end if;
+
+    end;
+end;
+/
+
+
+
+declare
+    /*v_value varchar2(100) := 'string';*/
+    v_value number := 5;
+begin
+
+   lazorenko_al.pkg_fail.fail_method(
+        p_value => v_value,
+        p_need_handle => true
+    );
+
+    commit;
+
+exception
+    when others then
+
+       lazorenko_al.add_error_log(
+            $$plsql_unit_owner||'.'||$$plsql_unit,
+            '{"error":"' || sqlerrm
+            ||'","value":"' || v_value
+            ||'","backtrace":"' || dbms_utility.format_error_backtrace()
+            ||'"}'
+        );
+
+end;
---
+++ test 6.3.sql
@@ -0,0 +1,27 @@
+select *
+    from (
+        select
+            er.id_log,
+            er.sh_dt,
+            er.object_name,
+            er.params,
+
+            (select error
+            from json_table(er.params, '$' columns (
+                error varchar2(100) path '$.error'
+            ))) error,
+
+            (select value
+            from json_table(er.params, '$' columns (
+                value varchar2(100) path '$.value'
+            ))) value
+
+        from (
+            select *
+            from lazorenko_al.error_log er
+            where er.params like '{"%' --если у вас всё-таки будет там лежать и не nosql в перемешку
+           -- and trunc(er.sh_dt) = trunc(to_date('18.11.2021','dd.mm.yyyy'))
+        ) er
+    ) jt
+--where jt.params like '%string%'
+;
