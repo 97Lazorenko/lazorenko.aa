08afb74c5add4bcec52281412ef95c0fc8605b1a
97Lazorenko <97Lazorenko97@gmail.com>
2021-11-24 13:42:19 +0700

    Учебный проект Лазоренко Александр

--- PLSQL 2 - отлов ошибок и запрос.sql
+++ PLSQL 2 - отлов ошибок и запрос.sql
@@ -5,15 +5,18 @@ create or replace procedure lazorenko_al.add_error_log(
     p_object_name varchar2,
     p_params varchar2,
     p_log_type varchar2 default 'common'
-)
+    )
 as
-pragma autonomous_transaction;
+    pragma autonomous_transaction;
+
 begin
 
     insert into lazorenko_al.error_log(object_name, log_type, params)
+
     values (p_object_name, p_log_type, p_params);
 
     commit;
+
 end;
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -22,41 +25,52 @@ end;
 
 --ПРОВЕРКА ПОЛА С ОТЛОВОМ ОШИБОК И ЛОГИРОВАНИЕМ
 
-    create or replace function sex_check(
+create or replace function sex_check(
     p_patient_id in number,
     p_spec_id in number)
-    return boolean as
+return boolean as
     v_sex number;
     v_count number;
     v_patient lazorenko_al.patient%rowtype;
+
     begin
+
     v_patient:=lazorenko_al.get_patient_info_by_id(p_patient_id);
     v_sex:=v_patient.sex_id;
+
     select count(*)
-    into v_count
-    from lazorenko_al.specialisation s
-    where s.spec_id=p_spec_id and (s.sex_id=v_sex or s.sex_id is null);
+        into v_count
+        from lazorenko_al.specialisation s
+        where s.spec_id=p_spec_id and (s.sex_id=v_sex or s.sex_id is null);
+
     return v_count>0;
+
     exception
+
         when no_data_found then lazorenko_al.add_error_log(
                 $$plsql_unit_owner||'.'||$$plsql_unit,
                 '{"error":"' || sqlerrm
                 ||'","value":"' || p_patient_id
                 ||'","backtrace":"' || dbms_utility.format_error_backtrace()
                 ||'"}'
-            );
+        );
+
         dbms_output.put_line('данный пациент отсутствует в базе больницы');
+
     return v_count>0;
+
     end;
 
 --ИСПОЛЬЗОВАНИЕ
 
-    declare
-v_check number;
+declare
+    v_check number;
 begin
-v_check:=sys.diutil.bool_to_int(lazorenko_al.sex_check(
+    v_check:=sys.diutil.bool_to_int(lazorenko_al.sex_check(
     9,5)); --ПРОВЕРКА ПОЛА
-dbms_output.put_line(v_check);
+
+    dbms_output.put_line(v_check);
+
 end;
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -67,20 +81,24 @@ end;
 create or replace function check_age(
     p_patient_id in number,
     p_spec_id in number)
-    return boolean as
+return boolean as
     v_patient lazorenko_al.patient%rowtype;
     v_age number;
     v_count number;
+
     begin
     v_patient:=lazorenko_al.get_patient_info_by_id(p_patient_id);
     v_age:=lazorenko_al.calculate_age_from_date(v_patient.born_date);
-    select count(*)
-    into v_count
-    from lazorenko_al.specialisation s
-    where s.spec_id = p_spec_id
-          and (s.min_age <= v_age or s.min_age is null)
-          and (s.max_age >= v_age or s.max_age is null);
+
+        select count(*)
+        into v_count
+        from lazorenko_al.specialisation s
+        where s.spec_id = p_spec_id
+               and (s.min_age <= v_age or s.min_age is null)
+               and (s.max_age >= v_age or s.max_age is null);
+
     return v_count>0;
+
     exception
         when no_data_found then lazorenko_al.add_error_log(
                 $$plsql_unit_owner||'.'||$$plsql_unit,
@@ -88,125 +106,34 @@ create or replace function check_age(
                 ||'","value":"' || p_patient_id
                 ||'","backtrace":"' || dbms_utility.format_error_backtrace()
                 ||'"}'
-            );
+        );
+
         dbms_output.put_line('данный пациент отсутствует в базе больницы');
+
     return v_count>0;
     end;
 
 --ИСПОЛЬЗОВАНИЕ
 
 declare
-v_check number;
+    v_check number;
 begin
-v_check:=sys.diutil.bool_to_int(lazorenko_al.check_age(
+    v_check:=sys.diutil.bool_to_int(lazorenko_al.check_age(
     10,5)); --ПРОВЕРКА ВОЗРАСТА
-dbms_output.put_line(v_check);
+
+    dbms_output.put_line(v_check);
 end;
 
 ------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------
 
-create or replace function write_to_records(
-         p_patient_id in number,
-         p_ticket_id in number,
-         p_need_handle boolean
-    )
-    return number as
-    v_record_id number;
-    no_patient_found_or_wrong_ticket exception;
-    pragma exception_init (no_patient_found_or_wrong_ticket, -02291);
-    begin
-    if (p_need_handle) then
-            begin
-            insert into lazorenko_al.records(record_id, record_stat_id, patient_id, ticket_id)
-            values (default, lazorenko_al.pkg_const_for_write_or_cancel.c_constant_1, p_patient_id, p_ticket_id)
-            returning ticket_id into v_record_id;
-
-            update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_const_for_write_or_cancel.c_constant_2 where t.ticket_id=p_ticket_id;
-
-            exception
-            when no_patient_found_or_wrong_ticket then lazorenko_al.add_error_log(
-                $$plsql_unit_owner||'.'||$$plsql_unit,
-                '{"error":"' || sqlerrm
-                ||'","value":"' || p_patient_id
-                ||'","backtrace":"' || dbms_utility.format_error_backtrace()
-                ||'"}'
-                );
-                dbms_output.put_line('запись внести невозможно - укажите другого пациента или талон');
-            rollback;
-            return v_record_id;
-            end;
-            else
-            insert into lazorenko_al.records(record_id, record_stat_id, patient_id, ticket_id)
-            values (default, lazorenko_al.pkg_const_for_write_or_cancel.c_constant_1, p_patient_id, p_ticket_id)
-            returning ticket_id into v_record_id;
-
-            update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_const_for_write_or_cancel.c_constant_2 where t.ticket_id=p_ticket_id;
-    end if;
-            return v_record_id;
-    end;
-
-
-declare
-    v_patient number := 10;
-    v_ticket number :=100;
-    v_record number;
-begin
-   v_record:=lazorenko_al.write_to_records(
-        p_patient_id => v_patient,
-        p_ticket_id => v_ticket,
-        p_need_handle => true
-    );
-    commit;
-end;
-
-
-    create or replace function cancel_record(
-    p_ticket_id in number)
-    return  number  as
-    v_record_id number;
-    no_ticket_found exception;
-    pragma exception_init (no_ticket_found, -20500);
-    begin
-    update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_const_for_write_or_cancel.c_constant_1 where t.ticket_id=p_ticket_id;
-
-    update lazorenko_al.records r set r.record_stat_id=lazorenko_al.pkg_const_for_write_or_cancel.c_constant_2 where r.ticket_id=p_ticket_id;
-
-    if sql%notfound
-        then raise_application_error(-20500,'указан неверный талон');
-    end if;
-     return v_record_id;
-    exception
-        when no_ticket_found then lazorenko_al.add_error_log(
-                $$plsql_unit_owner||'.'||$$plsql_unit,
-                '{"error":"' || sqlerrm
-                ||'","value":"' || p_ticket_id
-                ||'","backtrace":"' || dbms_utility.format_error_backtrace()
-                ||'"}'
-                );
-                dbms_output.put_line('указан неверный номер талона');
-        return v_record_id;
-    end;
-
-declare
-    v_ticket number :=111;
-    v_record number;
-begin
-   v_record:=lazorenko_al.cancel_record(
-        p_ticket_id => v_ticket
-    );
-    commit;
-end;
-
-------------------------------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------------------------------
 
 --ПАКЕТ С ФУНКЦИЯМИ ЗАПИСИ И ОТМЕНЫ ЗАПИСИ С ОТЛОВОМ ОШИЮОК И ЛОГИРОВАНИЕМ
 
 create or replace package lazorenko_al.pkg_write_or_cancel
 as
     c_record_or_ticket_stat_first constant number := 1;
-                                          --КОНСТАНТЫ БУДУТ ИСПОЛЬЗОВАНЫ В ФУНКЦИЯХ ЗАПИСИ И ОТМЕНЫ
+                                                          --КОНСТАНТЫ БУДУТ ИСПОЛЬЗОВАНЫ В ФУНКЦИЯХ ЗАПИСИ И ОТМЕНЫ
     c_record_or_ticket_stat_second constant number := 2;
 
     v_record_id lazorenko_al.records.record_id%type;
@@ -232,70 +159,102 @@ as
          p_need_handle boolean
     )
     return number as
-    v_record_id number;
-    no_patient_found_or_wrong_ticket exception;
-    pragma exception_init (no_patient_found_or_wrong_ticket, -02291);
-    begin
-    if (p_need_handle) then
-            begin
-            insert into lazorenko_al.records(record_id, record_stat_id, patient_id, ticket_id)
-            values (default, lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_first, p_patient_id, p_ticket_id)
-            returning ticket_id into v_record_id;
+        v_record_id number;
 
-            update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_second where t.ticket_id=p_ticket_id;
+        no_patient_found_or_wrong_ticket exception;
+        pragma exception_init (no_patient_found_or_wrong_ticket, -02291);
+
+    begin
+        if (p_need_handle) then
+            begin
+                insert into lazorenko_al.records(record_id, record_stat_id, patient_id, ticket_id)
+                values (default, lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_first, p_patient_id, p_ticket_id)
+                returning ticket_id into v_record_id;
+
+                update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_second
+                where t.ticket_id=p_ticket_id;
 
             exception
-            when no_patient_found_or_wrong_ticket then lazorenko_al.add_error_log(
-                $$plsql_unit_owner||'.'||$$plsql_unit||'.'||utl_call_stack.subprogram(1)(2),
-                '{"error":"' || sqlerrm
-                ||'","value":"' || p_patient_id
-                ||'","backtrace":"' || dbms_utility.format_error_backtrace()
-                ||'"}'
-                );
-                dbms_output.put_line('запись внести невозможно - укажите другого пациента или талон');
-            rollback;
-            return v_record_id;
+                when no_patient_found_or_wrong_ticket then
+
+                    lazorenko_al.add_error_log(
+        $$plsql_unit_owner||'.'||$$plsql_unit||'.'||utl_call_stack.subprogram(1)(2),
+            '{"error":"' || sqlerrm ||'","value":"'
+                     || p_patient_id
+                     ||'","backtrace":"' || dbms_utility.format_error_backtrace()
+                     ||'"}'
+                    );
+
+                    dbms_output.put_line('запись внести невозможно - укажите другого пациента или талон');
+
+                rollback;
+
+                return v_record_id;
+
             end;
-            else
+        else
+
             insert into lazorenko_al.records(record_id, record_stat_id, patient_id, ticket_id)
+
             values (default, lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_first, p_patient_id, p_ticket_id)
+
             returning ticket_id into v_record_id;
 
-            update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_second where t.ticket_id=p_ticket_id;
-    end if;
+            update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_second
+            where t.ticket_id=p_ticket_id;
+
+        end if;
+
             return v_record_id;
+
     end;
 
     function cancel_record(
-    p_ticket_id in number)
+        p_ticket_id in number
+    )
     return  number  as
-    v_record_id number;
-    no_ticket_found exception;
-    pragma exception_init (no_ticket_found, -20500);
+        v_record_id number;
+
+        no_ticket_found exception;
+        pragma exception_init (no_ticket_found, -20500);
+
     begin
-    update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_first where t.ticket_id=p_ticket_id;
 
-    update lazorenko_al.records r set r.record_stat_id=lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_second where r.ticket_id=p_ticket_id;
+            update lazorenko_al.ticket t set t.ticket_stat_id=lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_first
+            where t.ticket_id=p_ticket_id;
 
-    if sql%notfound
-        then raise_application_error(-20500,'указан неверный талон');
-    end if;
-     return v_record_id;
-    exception
-        when no_ticket_found then lazorenko_al.add_error_log(
-                $$plsql_unit_owner||'.'||$$plsql_unit||'.'||utl_call_stack.subprogram(1)(2),
-                '{"error":"' || sqlerrm
-                ||'","value":"' || p_ticket_id
-                ||'","backtrace":"' || dbms_utility.format_error_backtrace()
-                ||'"}'
-                );
-                dbms_output.put_line('указан неверный номер талона');
+            update lazorenko_al.records r set r.record_stat_id=lazorenko_al.pkg_write_or_cancel.c_record_or_ticket_stat_second
+            where r.ticket_id=p_ticket_id;
+
+        if sql%notfound then
+
+            raise_application_error(-20500,'указан неверный талон');
+
+        end if;
+
         return v_record_id;
+
+    exception
+        when no_ticket_found then
+
+            lazorenko_al.add_error_log(
+        $$plsql_unit_owner||'.'||$$plsql_unit||'.'||utl_call_stack.subprogram(1)(2),
+            '{"error":"' || sqlerrm
+                      ||'","value":"' || p_ticket_id
+                      ||'","backtrace":"' || dbms_utility.format_error_backtrace()
+                      ||'"}'
+            );
+
+            dbms_output.put_line('указан неверный номер талона');
+
+        return v_record_id;
+
     end;
+
 end;
 
 
---ИСПОЛЬЗОВАНИЕ
+----------------------------------------------------ИСПОЛЬЗОВАНИЕ-------------------------------------------------------
 
 --ЗАПИСЬ
 declare
--- PLSQL 3 - check -функция со сбросом ошибок.sql
+++ PLSQL 3 - check -функция со сбросом ошибок.sql
@@ -5,63 +5,84 @@
 create or replace function check_age(
     p_patient_id in number,
     p_spec_id in number)
-    return boolean as
+return boolean as
     v_patient lazorenko_al.patient%rowtype;
     v_age number;
     v_count number;
+
     e_wrong_age exception;
     pragma exception_init (e_wrong_age, -20400);
+
     begin
+
     v_patient:=lazorenko_al.get_patient_info_by_id(p_patient_id);
     v_age:=lazorenko_al.calculate_age_from_date(v_patient.born_date);
-    select count(*)
-    into v_count
-    from lazorenko_al.specialisation s
-    where s.spec_id = p_spec_id
-          and (s.min_age <= v_age or s.min_age is null)
-          and (s.max_age >= v_age or s.max_age is null);
-    if v_count=0 then raise_application_error (-20400, 'возраст пациента не соответствует полу специальности');
+
+        select count(*)
+        into v_count
+        from lazorenko_al.specialisation s
+        where s.spec_id = p_spec_id
+              and (s.min_age <= v_age or s.min_age is null)
+              and (s.max_age >= v_age or s.max_age is null);
+
+    if v_count=0 then
+       raise_application_error (-20400, 'возраст пациента не соответствует полу специальности');
+
     end if;
+
     return v_count>0;
+
     exception
-        when no_data_found then lazorenko_al.add_error_log(
-                $$plsql_unit_owner||'.'||$$plsql_unit,
-                '{"error":"' || sqlerrm
-                ||'","value":"' || p_patient_id
-                ||'","backtrace":"' || dbms_utility.format_error_backtrace()
-                ||'"}'
-            );
+        when no_data_found then
+            lazorenko_al.add_error_log(
+    $$plsql_unit_owner||'.'||$$plsql_unit,
+        '{"error":"' || sqlerrm
+                  ||'","value":"' || p_patient_id
+                  ||'","backtrace":"' || dbms_utility.format_error_backtrace()
+                  ||'"}'
+        );
+
         dbms_output.put_line('данный пациент отсутствует в базе больницы');
-    return v_count>0;
-        when e_wrong_age then lazorenko_al.add_error_log(
-                $$plsql_unit_owner||'.'||$$plsql_unit,
-                '{"error":"' || sqlerrm
-                ||'","value":"' || p_patient_id
-                ||'","backtrace":"' || dbms_utility.format_error_backtrace()
-                ||'"}'
+
+        return v_count>0;
+
+        when e_wrong_age then
+            lazorenko_al.add_error_log(
+    $$plsql_unit_owner||'.'||$$plsql_unit,
+        '{"error":"' || sqlerrm
+                  ||'","value":"' || p_patient_id
+                  ||'","backtrace":"' || dbms_utility.format_error_backtrace()
+                  ||'"}'
             );
-        dbms_output.put_line('возраст пациента не соответствует полу специальности');
-    return v_count>0;
+
+            dbms_output.put_line('возраст пациента не соответствует полу специальности');
+
+        return v_count>0;
+
     end;
 
 --ПРИМЕНЕНИЕ ФУНКЦИИ - ВВОДИМ ID ПАЦИЕНТА, ОТСУТСТВУЮЩЕГО В БД
 
 declare
-v_check number;
+    v_check number;
 begin
-v_check:=sys.diutil.bool_to_int(lazorenko_al.check_age(
+    v_check:=sys.diutil.bool_to_int(lazorenko_al.check_age(
     10,6));
-dbms_output.put_line(v_check);
+
+    dbms_output.put_line(v_check);
+
 end;
 
 --ПРИМЕНЕНИЕ ФУНКЦИИ - ВВОДИМ ID ПАЦИЕНТА, НЕ ОТВЕЧАЮЩЕГО ВОЗРАСТНЫМ ОГРАНИЧЕНИЯМ ПО СПЕЦИАЛЬНОСТИ
 
 declare
-v_check number;
+    v_check number;
 begin
-v_check:=sys.diutil.bool_to_int(lazorenko_al.check_age(
+    v_check:=sys.diutil.bool_to_int(lazorenko_al.check_age(
     7,6));
-dbms_output.put_line(v_check);
+
+    dbms_output.put_line(v_check);
+
 end;
 
 
@@ -72,20 +93,22 @@ end;
 create or replace function lazorenko_al.check_for_accept_with_exceptions(
     p_patient_id in number,
     p_spec_id number
-)
+    )
 return boolean
 as
     v_result boolean := true;
-begin
+
+    begin
 
     if (not lazorenko_al.check_age(
         p_patient_id => p_patient_id,
         p_spec_id => p_spec_id
-    )) then v_result:=false;
+        ))
+    then v_result:=false;
     end if;
 
-return v_result;
-end;
+    return v_result;
+    end;
 
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -97,54 +120,61 @@ create or replace function lazorenko_al.accept_record_by_rules_with_exceptions(
     v_patient_id number,
     v_spec_id number,
     v_result out number,
-
     v_need_handle boolean)
 return  number as
     v_record_id number;
-begin
- if (lazorenko_al.check_for_accept_with_exceptions(
-    p_patient_id => v_patient_id,
-    p_spec_id => v_spec_id))
- then v_record_id:=lazorenko_al.pkg_write_or_cancel.write_to_records(
-    p_patient_id => v_patient_id,
-    p_ticket_id => v_ticket_id,
-    p_need_handle => v_need_handle);
+    begin
+        if (lazorenko_al.check_for_accept_with_exceptions(
+            p_patient_id => v_patient_id,
+            p_spec_id => v_spec_id))
 
-end if;
-if sql%found then dbms_output.put_line(v_record_id ||' - '||'запись осуществлена успешно');
-end if;
+        then v_record_id:=lazorenko_al.pkg_write_or_cancel.write_to_records(        --    В данной функции тожу обрабатываются
+            p_patient_id => v_patient_id,                                           --    ошибки. Ознакомиться с данным пакетом
+            p_ticket_id => v_ticket_id,                                             --    и функцией можно в PLSQL 2
+            p_need_handle => v_need_handle);
 
-return v_result;
-end;
+        end if;
+
+        if sql%found then
+
+        dbms_output.put_line(v_record_id ||' - '||'запись осуществлена успешно');
+
+        end if;
+
+    return v_result;
+    end;
 
 --ПРИМЕНЕНИЕ ФУНКЦИИ - ВВОДИМ ID ПАЦИЕНТА, ОТСУТСТВУЮЩЕГО В БД
 
 declare
-
     v_result number;
 begin
     v_result:=lazorenko_al.accept_record_by_rules_with_exceptions(33, 10, 6, v_result, true);
+
     commit;
+
 end;
 
 --ПРИМЕНЕНИЕ ФУНКЦИИ - ВВОДИМ ID ПАЦИЕНТА, НЕ ОТВЕЧАЮЩЕГО ВОЗРАСТНЫМ ОГРАНИЧЕНИЯМ ПО СПЕЦИАЛЬНОСТИ
 
 declare
-
     v_result number;
 begin
     v_result:=lazorenko_al.accept_record_by_rules_with_exceptions(33, 7, 6, v_result, true);
+
     commit;
+
 end;
 
 --ПРИМЕНЕНИЕ ФУНКЦИИ - ВВОДИМ ID ТАЛОНА, ОТСУТСТВУЮЩЕГО В БД
 
 declare
-
     v_result number;
 begin
     v_result:=lazorenko_al.accept_record_by_rules_with_exceptions(100, 3, 6, v_result, true);
+
     commit;
+
 end;
 
 
