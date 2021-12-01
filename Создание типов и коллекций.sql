6d53b22cf7baaa8684d0af45829587acb2df54c9
97Lazorenko <97Lazorenko97@gmail.com>
2021-12-01 09:58:35 +0700

    Учебный проект Лазоренко Александр

--- Создание типов и коллекций.sql
+++ Создание типов и коллекций.sql
@@ -1,5 +1,5 @@
 --специальность
-create or replace type lazorenko_al.t_specialisation as object(
+create or replace type lazorenko_al.t_specialisation as object(  --вместо row%type
     spec_id number,
     name varchar2(100),
     enter_into_the_sys date,
@@ -8,17 +8,17 @@ create or replace type lazorenko_al.t_specialisation as object(
     max_age number,
     sex_id number);
 
-create or replace type lazorenko_al.t_arr_specialisation as table of lazorenko_al.t_specialisation;
+create or replace type lazorenko_al.t_arr_specialisation as table of lazorenko_al.t_specialisation; --массив
 
 --пациент
-create or replace type lazorenko_al.t_patient as object(
+create or replace type lazorenko_al.t_patient as object(  --вместо row%type
     patient_id number,
     born_date date,
     sex_id number
 );
 
---добавление собственного конструктора
-alter type lazorenko_al.t_patient
+
+alter type lazorenko_al.t_patient  --добавление собственного конструктора
 add constructor function t_patient(
     patient_id number,
     born_date date,
@@ -26,7 +26,7 @@ add constructor function t_patient(
 ) return self as result
 cascade;
 
-create or replace type body lazorenko_al.t_patient
+create or replace type body lazorenko_al.t_patient  --его тело
 as
     constructor function t_patient(
         patient_id number,
@@ -43,17 +43,17 @@ as
     end;
 end;
 
-create or replace type lazorenko_al.t_arr_patient as table of lazorenko_al.t_patient;
+create or replace type lazorenko_al.t_arr_patient as table of lazorenko_al.t_patient;  --массив
 
 --документы пациента
-create or replace type lazorenko_al.t_documents_numbers as object(
+create or replace type lazorenko_al.t_documents_numbers as object(  --вместо row%type
     doc_num_id number,
     patient_id number,
     document_id number,
     value varchar2(50)
 );
 
-create or replace type lazorenko_al.t_arr_documents_numbers as table of lazorenko_al.t_documents_numbers;
+create or replace type lazorenko_al.t_arr_documents_numbers as table of lazorenko_al.t_documents_numbers;  --массив
 
 --расширенный пациент (с его документами)
 
@@ -62,7 +62,7 @@ create or replace type lazorenko_al.t_extended_patient as object(
     documents_numbers lazorenko_al.t_arr_documents_numbers);
 
 --доктор
-create or replace type lazorenko_al.t_doctor as object(
+create or replace type lazorenko_al.t_doctor as object(  --вместо row%type
     doctor_id number,
     name varchar2(100),
     hospital_id number,
@@ -70,10 +70,10 @@ create or replace type lazorenko_al.t_doctor as object(
     hiring_date date,
     dismiss_date date);
 
-create or replace type lazorenko_al.t_arr_doctor as table of lazorenko_al.t_doctor;
+create or replace type lazorenko_al.t_arr_doctor as table of lazorenko_al.t_doctor;  --массив
 
 --регалии доктора
-create or replace type lazorenko_al.t_doctor_info as object(
+create or replace type lazorenko_al.t_doctor_info as object(  --вместо row%type
     doctor_info_id number,
     education varchar2(100),
     qualification number,
@@ -82,7 +82,7 @@ create or replace type lazorenko_al.t_doctor_info as object(
     reviews varchar2(500),
     doctor_id number);
 
-create or replace type lazorenko_al.t_arr_doctor_info as table of lazorenko_al.t_doctor_info;
+create or replace type lazorenko_al.t_arr_doctor_info as table of lazorenko_al.t_doctor_info;  --массив
 
 --расширенный доктор (с его регалиями)
 create or replace type lazorenko_al.t_extended_doctor as object(
@@ -90,7 +90,7 @@ create or replace type lazorenko_al.t_extended_doctor as object(
     doctors_info lazorenko_al.t_arr_doctor_info);
 
 --больница
-create or replace type lazorenko_al.t_hospital as object(
+create or replace type lazorenko_al.t_hospital as object(  --вместо row%type
     hospital_id number,
     name varchar2(100),
     availability_id number,
@@ -99,17 +99,17 @@ create or replace type lazorenko_al.t_hospital as object(
     enter_into_the_sys date,
     delete_from_the_sys date);
 
-create or replace type lazorenko_al.t_arr_hospital as table of lazorenko_al.t_hospital;
+create or replace type lazorenko_al.t_arr_hospital as table of lazorenko_al.t_hospital;  --массив
 
 --расписание больницы
-create or replace type lazorenko_al.t_work_time as object(
+create or replace type lazorenko_al.t_work_time as object(  --вместо row%type
     work_time_id number,
     day number,
     begin_time varchar2(100),
     end_time varchar2(100),
     hospital_id number);
 
-create or replace type lazorenko_al.t_arr_work_time as table of lazorenko_al.t_work_time;
+create or replace type lazorenko_al.t_arr_work_time as table of lazorenko_al.t_work_time;  --массив
 
 --расширенная больница (с ее расписанием)
 create or replace type lazorenko_al.t_extended_hospital as object(
@@ -117,23 +117,23 @@ create or replace type lazorenko_al.t_extended_hospital as object(
     work_time lazorenko_al.t_arr_work_time);
 
 --талон
-create or replace type lazorenko_al.t_tickets as object(
+create or replace type lazorenko_al.t_tickets as object(  --вместо row%type
     ticket_id number,
     doctor_id number,
     ticket_stat_id number,
     appointment_beg varchar2(50),
     appointment_end varchar2(50));
 
-create or replace type lazorenko_al.t_arr_tickets as table of lazorenko_al.t_tickets;
+create or replace type lazorenko_al.t_arr_tickets as table of lazorenko_al.t_tickets;  --массив
 
 
 --журнальный талон
-create or replace type lazorenko_al.t_record as object(
+create or replace type lazorenko_al.t_record as object(  --вместо row%type
     record_id number,
     record_stat_id number,
     patient_id number,
     ticket_id number);
 
-create or replace type lazorenko_al.t_arr_record as table of lazorenko_al.t_record;
+create or replace type lazorenko_al.t_arr_record as table of lazorenko_al.t_record;  --массив
 
 
