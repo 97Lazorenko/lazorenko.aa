8fb7e3742f6eb9c9e3f3b233449fa01f13ab8bd2
97Lazorenko <97Lazorenko97@gmail.com>
2021-12-09 02:42:14 +0700

    Учебный проект Лазоренко Александр

---
+++ Пакеты с репозиториями.sql
@@ -0,0 +1,266 @@
+--пакет с репозиторием докторов
+create or replace package lazorenko_al.pkg_doctor_repository
+as
+    function repository(
+    out_result out number
+    )
+    return clob;
+end;
+
+--его тело
+create or replace package body lazorenko_al.pkg_doctor_repository
+as
+    function repository(
+    out_result out number
+    )
+    return clob
+    as
+
+    v_success boolean;
+    v_code number;
+    v_clob clob;
+
+    begin
+
+    v_clob := lazorenko_al.HTTP_FETCH(
+        p_url => 'http://virtserver.swaggerhub.com/AntonovAD/DoctorDB/1.0.0/doctors',
+        p_debug => true,
+        out_success => v_success,
+        out_code => v_code
+    );
+
+    out_result := case when v_success
+        then lazorenko_al.pkg_code.c_ok
+        else lazorenko_al.pkg_code.c_error
+    end;
+
+    return v_clob;
+
+    end;
+end;
+
+
+--пакет с репозиторием больниц
+create or replace package lazorenko_al.pkg_hospital_repository
+as
+    function repository_hospitals(
+    out_result out number
+    )
+    return clob;
+end;
+
+--его тело
+create or replace package body lazorenko_al.pkg_hospital_repository
+as
+    function repository_hospitals(
+    out_result out number
+    )
+    return clob
+    as
+
+    v_success boolean;
+    v_code number;
+    v_clob clob;
+
+    begin
+
+    v_clob := lazorenko_al.HTTP_FETCH(
+        p_url => 'http://virtserver.swaggerhub.com/AntonovAD/DoctorDB/1.0.0/hospitals',
+        p_debug => true,
+        out_success => v_success,
+        out_code => v_code
+    );
+
+    out_result := case when v_success
+        then lazorenko_al.pkg_code.c_ok
+        else lazorenko_al.pkg_code.c_error
+    end;
+
+    return v_clob;
+
+    end;
+end;
+
+
+--пакет с репозиторием специальностей
+create or replace package lazorenko_al.pkg_specs_repository
+as
+    function repository_specs(
+    out_result out number
+    )
+    return clob;
+end;
+
+--его тело
+create or replace package body lazorenko_al.pkg_specs_repository
+as
+    function repository_specs(
+    out_result out number
+    )
+    return clob
+    as
+
+    v_success boolean;
+    v_code number;
+    v_clob clob;
+
+    begin
+
+    v_clob := lazorenko_al.HTTP_FETCH(
+        p_url => 'http://virtserver.swaggerhub.com/AntonovAD/DoctorDB/1.0.0/specialties',
+        p_debug => true,
+        out_success => v_success,
+        out_code => v_code
+    );
+
+    out_result := case when v_success
+        then lazorenko_al.pkg_code.c_ok
+        else lazorenko_al.pkg_code.c_error
+    end;
+
+    return v_clob;
+
+    end;
+end;
+
+
+--пакет с репозиториями записи/отмены записи
+create or replace package lazorenko_al.pkg_write_or_cancel_repository
+as
+
+    function repository_recording(
+    p_ticket_id number,
+    p_patient_id number
+    )
+    return lazorenko_al.t_result;
+
+    function repository_cancel_record(
+    p_ticket_id number
+    )
+    return lazorenko_al.t_result;
+
+end;
+
+--тело пакета
+create or replace package body lazorenko_al.pkg_write_or_cancel_repository
+as
+    function repository_recording(
+    p_ticket_id number,
+    p_patient_id number
+    )
+    return lazorenko_al.t_result as
+        v_result lazorenko_al.t_result;
+
+    begin
+        v_result:=lazorenko_al.pkg_write_or_cancel.write_to_records(
+        p_patient_id => p_patient_id,
+        p_ticket_id => p_ticket_id,
+        p_need_handle => true
+    );
+    return v_result;
+
+    end;
+
+    function repository_cancel_record(
+    p_ticket_id number
+    )
+    return lazorenko_al.t_result as
+        v_result lazorenko_al.t_result;
+
+    begin
+        v_result:=lazorenko_al.pkg_write_or_cancel.cancel_record(
+        p_ticket_id => p_ticket_id
+        );
+    return v_result;
+
+    end;
+end;
+
+--пакет с проверками условий перед записью
+create or replace package lazorenko_al.pkg_check_before_write_repository
+as
+    function repository_with_checks(
+    p_ticket_id number,
+    p_patient_id number,
+    p_spec_id number,
+    p_doctor_id number,
+    p_hospital_id number,
+    out_result out number
+    )
+    return number;
+end;
+
+--его тело
+create or replace package body lazorenko_al.pkg_check_before_write_repository
+as
+    function repository_with_checks(
+    p_ticket_id number,
+    p_patient_id number,
+    p_spec_id number,
+    p_doctor_id number,
+    p_hospital_id number,
+    out_result out number
+    )
+    return number
+    as v_result boolean;
+
+    begin
+
+    v_result:=lazorenko_al.check_for_accept_with_exceptions(
+    p_ticket_id => p_ticket_id,
+    p_patient_id => p_patient_id,
+    p_spec_id => p_spec_id,
+    p_doctor_id => p_doctor_id,
+    p_hospital_id => p_hospital_id);
+
+    out_result := case when sys.diutil.bool_to_int(v_result)>0
+        then lazorenko_al.pkg_code.c_ok
+        else lazorenko_al.pkg_code.c_error
+    end;
+
+    return out_result;
+
+    end;
+
+end;
+
+--пакет с проверками условий перед отменой записи
+create or replace package lazorenko_al.pkg_check_before_cancel_repository
+as
+    function repository_with_checks_before_cancel(
+    p_hospital_id number,
+    p_ticket_id number,
+    p_patient_id number,
+    out_result out number
+    )
+    return number;
+end;
+
+--его тело
+create or replace package body lazorenko_al.pkg_check_before_cancel_repository
+as
+    function repository_with_checks_before_cancel(
+    p_hospital_id number,
+    p_ticket_id number,
+    p_patient_id number,
+    out_result out number
+    )
+    return number
+    as v_result boolean;
+
+    begin
+
+    v_result:=lazorenko_al.check_cancel(
+    p_hospital_id => p_hospital_id,
+    p_ticket_id => p_ticket_id,
+    p_patient_id => p_patient_id);
+
+    out_result := case when sys.diutil.bool_to_int(v_result)>0
+        then lazorenko_al.pkg_code.c_ok
+        else lazorenko_al.pkg_code.c_error
+    end;
+
+    return out_result;
+
+    end;
+end;
---
+++ Сервисы для джобов.sql
@@ -0,0 +1,216 @@
+/*
+ PARSING
+ */
+
+create or replace type lazorenko_al.t_doctor as object(
+    doctor_id number,
+    hospital_id number,
+    name varchar(100),
+    fname varchar(50),
+    petronymic varchar2(50)
+);
+
+create or replace type lazorenko_al.t_arr_doctor as table of lazorenko_al.t_doctor;
+
+
+
+--сервис доктора
+create or replace function lazorenko_al.service_for_doctors(
+    out_result out number
+)
+return lazorenko_al.t_arr_doctor
+as
+
+    v_result integer;
+    v_clob clob;
+    v_response lazorenko_al.t_arr_doctor := lazorenko_al.t_arr_doctor();
+
+begin
+
+    v_clob := lazorenko_al.pkg_doctor_repository.repository(
+        out_result => v_result
+    );
+
+
+    select lazorenko_al.t_doctor(
+        doctor_id => r.id_doctor,
+        hospital_id => r.id_hospital,
+        name => r.lname,
+        fname => r.fname,
+        petronymic => r.mname
+    )
+    bulk collect into v_response
+    from json_table(v_clob, '$' columns(
+        nested path '$[*]' columns(
+            id_doctor number path '$.id_doctor',
+            id_hospital number path '$.id_hospital',
+            lname varchar2(100) path '$.lname',
+            fname varchar2(100) path '$.fname',
+            mname varchar2(100) path '$.mname'
+    ))) r;
+
+    out_result := v_result;
+
+    return v_response;
+
+end;
+/
+
+
+--его применение
+
+declare
+
+    v_result integer;
+    v_response lazorenko_al.t_arr_doctor := lazorenko_al.t_arr_doctor();
+
+begin
+
+    v_response := lazorenko_al.service_for_doctors(
+        out_result => v_result
+    );
+
+    if v_response.count>0 then
+    for i in v_response.first..v_response.last
+    loop
+        declare
+            v_item lazorenko_al.t_doctor := v_response(i);
+        begin
+            dbms_output.put_line('id врача: '||v_item.doctor_id ||'  '||'id больницы: '||v_item.hospital_id||'  '||'ФИО: '|| v_item.name|| ' '||v_item.fname||' '||v_item.petronymic);
+        end;
+    end loop;
+    end if;
+
+end;
+
+
+--сервис больницы
+create or replace function lazorenko_al.service_for_hospitals(
+    out_result out number
+)
+return lazorenko_al.t_arr_hospitals
+as
+
+    v_result integer;
+    v_clob clob;
+    v_response lazorenko_al.t_arr_hospitals := lazorenko_al.t_arr_hospitals();
+
+begin
+
+    v_clob := lazorenko_al.pkg_hospital_repository.repository_hospitals(
+        out_result => v_result
+    );
+
+
+    select lazorenko_al.t_hospitals(
+        id_hospital => r.id_hospital,
+        name => r.name,
+        address => r.address,
+        id_town => r.id_town
+    )
+    bulk collect into v_response
+    from json_table(v_clob, '$' columns(
+        nested path '$[*]' columns(
+            id_hospital number path '$.id_hospital',
+            name varchar2(100) path '$.name',
+            address varchar2(100) path '$.address',
+            id_town number path '$.id_town'
+    ))) r;
+
+    out_result := v_result;
+
+    return v_response;
+
+end;
+
+--его применение
+
+declare
+
+    v_result integer;
+    v_response lazorenko_al.t_arr_hospitals := lazorenko_al.t_arr_hospitals();
+
+begin
+
+    v_response := lazorenko_al.service_for_hospitals(
+        out_result => v_result
+    );
+
+    if v_response.count>0 then
+    for i in v_response.first..v_response.last
+    loop
+        declare
+            v_item lazorenko_al.t_hospitals := v_response(i);
+        begin
+            dbms_output.put_line('id больницы: '||v_item.id_hospital ||'  '||'название больницы: '||v_item.name||'  '||'адрес: '|| v_item.address|| ';  id города:'||v_item.id_town);
+        end;
+    end loop;
+    end if;
+
+end;
+
+
+
+--сервис специальностей
+
+create or replace function lazorenko_al.service_for_specs(
+    out_result out number
+)
+return lazorenko_al.t_arr_new_specs
+as
+
+    v_result integer;
+    v_clob clob;
+    v_response lazorenko_al.t_arr_new_specs := lazorenko_al.t_arr_new_specs();
+
+begin
+
+    v_clob := lazorenko_al.pkg_specs_repository.repository_specs(
+        out_result => v_result
+    );
+
+
+    select lazorenko_al.t_new_specs(
+        id_specialty => r.id_specialty,
+        name => r.name,
+        id_hospital => r.id_hospital
+    )
+    bulk collect into v_response
+    from json_table(v_clob, '$' columns(
+        nested path '$[*]' columns(
+            id_specialty number path '$.id_specialty',
+            name varchar2(100) path '$.name',
+            id_hospital number path '$.id_hospital'
+    ))) r;
+
+    out_result := v_result;
+
+    return v_response;
+
+end;
+
+
+
+declare
+
+    v_result integer;
+    v_response lazorenko_al.t_arr_new_specs := lazorenko_al.t_arr_new_specs();
+
+begin
+
+    v_response := lazorenko_al.service_for_specs(
+        out_result => v_result
+    );
+
+    if v_response.count>0 then
+    for i in v_response.first..v_response.last
+    loop
+        declare
+            v_item lazorenko_al.t_new_specs := v_response(i);
+        begin
+            dbms_output.put_line('id специальности: '||v_item.id_hospital ||'  '||'название специальности: '||v_item.name||'  '||'id больницы:'||v_item.id_hospital);
+        end;
+    end loop;
+    end if;
+
+end;
